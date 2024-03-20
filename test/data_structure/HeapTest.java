a3cee37207e65cd279e42ab0dc4bf8a8840f2925
bbokyu <bbokyu23@gmail.com>
2024-03-19 18:47:37 -0700

    Init Heap

---
+++ test/data_structure/HeapTest.java
@@ -0,0 +1,17 @@
+package data_structure;
+
+import org.junit.jupiter.api.*;
+
+public class HeapTest {
+
+    @BeforeEach
+    public void setup() {}
+
+    @Test
+    public void testHeapConstruction() {
+        Heap heap = new Heap();
+
+    }
+
+
+}
